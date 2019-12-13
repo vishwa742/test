a2d0340e8b91e178332d6b8bb2badf3fb98c1bab
Vishwanath Ezhil <54872169+vishwa742@users.noreply.github.com>
2019-12-13 03:55:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,5 @@
 from flask import Flask
 
-from .config import app_config
-from .models import db, bcrypt
-
-# import user_api blueprint
-from .views.UserView import user_api as user_blueprint
-from .views.BlogpostView import blogpost_api as blogpost_blueprint
-
-
-
 app = Flask(__name__)
 
 @app.route('/')
